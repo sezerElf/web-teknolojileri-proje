5b649398baee4e0dff52ad2db657437cb51440c3
Elif Sezer <elifsezer724@gmail.com>
2020-04-19 19:22:33 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function temizle(){
+    document.getElementById("form1").value = null;
+    document.getElementById("form2").value = null;
+    document.getElementById("form3").value = null;
+    document.getElementById("form4").value = null;
+}
